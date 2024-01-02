fad9bb1a3be9cb7560d64abff9731bb534c78925
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-03 00:46:08 +0900

    ♻️ [refactor] 트랜잭션 추가

--- src/main/java/com/example/msg/service/QuestionService.java
+++ src/main/java/com/example/msg/service/QuestionService.java
@@ -6,22 +6,26 @@ import com.example.msg.repository.QuestionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
-@Slf4j
-@Service
+@Slf4j // 로깅
+@Service // 서비스
 public class QuestionService {
     @Autowired
     private QuestionRepository questionRepository;
 
+    // 주어진 AddQuestionRequest를 이용하여 질문을 생성하고 저장
+    @Transactional
     public Question create(AddQuestionRequest dto) {
         Question question = dto.toEntity();
-        System.out.println(dto.toEntity());
         log.info(dto.toString());
         return questionRepository.save(question);
     }
 
+    // 현재 저장된 모든 질문 목록을 조회
+    @Transactional(readOnly = true)
     public ArrayList<Question> list() {
         return questionRepository.findAll();
     }
