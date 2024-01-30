a57d2725776ee06a001cdcc12441d93276565e04
haeunNoh06 <okhagrace06@naver.com>
2024-01-30 21:18:13 +0900

    :sparkles: [feat] SupportForm Service 완성

--- src/main/java/com/example/msg/service/SupportFormService.java
+++ src/main/java/com/example/msg/service/SupportFormService.java
@@ -1,4 +1,25 @@
 package com.example.msg.service;
 
+import com.example.msg.domain.SupportForm;
+import com.example.msg.dto.AddSupportFormRequest;
+import com.example.msg.repository.SupportFormRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class SupportFormService {
+
+    private final SupportFormRepository supportFormRepository;
+
+    public SupportForm save(AddSupportFormRequest request) {
+        return supportFormRepository.save(request.toEntity());
+    }
+
+    public List<SupportForm> findAll() {
+        return supportFormRepository.findAll();
+    }
+
 }
