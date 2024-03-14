e836409934e5e0cfed22dc0f466684b4b411b119
mic050r <s2216@e-mirim.hs.kr>
2024-03-14 10:27:56 +0900

    ✨️ [feat] db 정보 수정 및 gitignore 추가

--- src/main/java/com/example/msg/config/WebConfig.java
+++ src/main/java/com/example/msg/config/WebConfig.java
@@ -1,30 +1,30 @@
-//package com.example.msg.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@RequiredArgsConstructor
-//@Configuration
-//@Service
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
-//                        .allowedHeaders("*")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-//                        .exposedHeaders("Authorization", "RefreshToken")
-//                        .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
-//            }
-//        };
-//    }
-//
-//}
+package com.example.msg.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@Service
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken")
+                        .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+            }
+        };
+    }
+
+}
